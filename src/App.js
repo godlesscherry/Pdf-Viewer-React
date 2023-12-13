098b30c7e11d9a3de8cc615d6648e55f80682a90
godlesscherry <haricharan.nitt@gmail.com>
2023-12-13 15:45:41 +0530

    refactor: style with material ui,react dom 17

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import DocumentViewer from './DocumentViewer';
+import { Button, Typography, Container } from '@material-ui/core';
 
 const App = () => {
     const [isViewerOpen, setIsViewerOpen] = useState(false);
     const documentFile = process.env.PUBLIC_URL + '/large-multi-page.pdf';
     const handleOpenViewer = () => setIsViewerOpen(true);
     const handleCloseViewer = () => setIsViewerOpen(false);
+
     return (
-        <div>
-            <button onClick={handleOpenViewer}>View Document</button>
+        <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '20px' }}>
+            <Typography variant="h4" gutterBottom>
+                PDF Viewer React App
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+                Simple App to view PDFs using react-pdf, handles password protected files and multi-page PDFs with navigation features and good UX.
+            </Typography>
+            <Button variant="contained" color="primary" onClick={handleOpenViewer}>
+                View Document
+            </Button>
             {isViewerOpen && (
                 <DocumentViewer file={documentFile} onClose={handleCloseViewer} />
             )}
-        </div>
+        </Container>
     );
 };
 
